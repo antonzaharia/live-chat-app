2685bc4e992ff0ebb2997ded9858fa65275a25d6
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 14:11:29 +0000

    feat: New Chat form
    Changes:
    - Added tom select stylesheet
    - Rendered the chat form in the caht new modal
    - Accepted nested attributes for user chats and messages in chat form

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -13,5 +13,7 @@ class Chat < ApplicationRecord
   has_many :user_chats, dependent: :destroy
   has_many :users, through: :user_chats
 
+  accepts_nested_attributes_for :user_chats, :messages
+
   validates :name, presence: true
 end
