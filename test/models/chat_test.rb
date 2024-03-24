f9ec68bc79b30d8a73d64a8d29007df2c528d1e7
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 13:31:55 +0000

    test: Chat model

--- test/models/chat_test.rb
+++ test/models/chat_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class ChatTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @chat = chats(:one)
+  end
+
+  test "should not save chat without name" do
+    chat = Chat.new
+    assert_not chat.save, "Saved the chat without a name"
+  end
+
+  test "should have many messages" do
+    assert_respond_to @chat, :messages
+  end
+
+  test "should have many user_chats" do
+    assert_respond_to @chat, :user_chats
+  end
+
+  test "should have many users through user_chats" do
+    assert_respond_to @chat, :users
+  end
 end
