f95acfd57b208ac631c28aefd47817f1aba03d7d
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 12:49:15 +0000

    feat: add name to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_24_120560) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_24_120721) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -29,6 +29,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_24_120560) do
     t.boolean "verified", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
