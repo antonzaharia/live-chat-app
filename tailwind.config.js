1d39b93f010ac0648ff67ee7c2b56793b862500b
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 12:29:25 +0000

    setup: Installed style dependencies
    Changes:
    - installed DaisyUi and all dependencies
    - setup configuration files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
+    './app/javascript/**/*.js',
+  ],
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'black',
+  },
 }
