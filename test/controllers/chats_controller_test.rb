064d951c9ce1b54f01ba1bfe936fce42e339e22f
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 13:46:07 +0000

    test: Chats Controller
    Test:
    - Show action response
    - New action response
    - Create action
    - Create validation

--- test/controllers/chats_controller_test.rb
+++ test/controllers/chats_controller_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class ChatsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = sign_in_as(users(:anton))
+    @chat = chats(:one)
+  end
+
+  test "should get show" do
+    get chat_url(@chat)
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_chat_url
+    assert_response :success
+  end
+
+  test "should create chat" do
+    assert_difference('Chat.count') do
+      post chats_url, params: { chat: { name: @chat.name } }
+    end
+
+    assert_redirected_to root_path
+  end
+
+  test "should not create invalid chat" do
+    assert_no_difference('Chat.count') do
+      post chats_url, params: { chat: { name: nil } }
+    end
+
+    assert_response :unprocessable_entity
+  end
 end
