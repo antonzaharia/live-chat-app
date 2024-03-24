a4a4dc118e3dc89caa9ef133c612abab85b8f2fe
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 13:32:09 +0000

    test: UserChat model

--- test/models/user_chat_test.rb
+++ test/models/user_chat_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserChatTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:anton)
+    @chat = chats(:one)
+  end
+
+  test "should belong to user" do
+    user_chat = UserChat.new(user: @user, chat: @chat)
+    assert_respond_to user_chat, :user
+  end
+
+  test "should belong to chat" do
+    user_chat = UserChat.new(user: @user, chat: @chat)
+    assert_respond_to user_chat, :chat
+  end
 end
