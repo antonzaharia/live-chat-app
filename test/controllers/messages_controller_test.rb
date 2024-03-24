8d7131d4cd053fb12a661515aad5d6b5a6343f12
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 14:46:52 +0000

    test: MessagesController tests
    Test:
    - message create action

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 class MessagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @chat = chats(:one)
+    @user = sign_in_as(users(:anton))
+  end
+
+  test "should create message" do
+    assert_difference('Message.count') do
+      post chat_messages_url(@chat), params: { message: { content: "Test content" } }
+    end
+
+    assert_redirected_to chat_path(@chat)
+  end
+
+  test "should not create invalid message" do
+    assert_no_difference('Message.count') do
+      post chat_messages_url(@chat), params: { message: { content: nil } }
+    end
+
+    assert_response :unprocessable_entity
+  end
 end
