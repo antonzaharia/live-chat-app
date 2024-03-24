276c650cf0d64ba891c36f9be35eefb387774103
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 13:32:01 +0000

    test: Message model

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,7 +1,28 @@
 require "test_helper"
 
 class MessageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:anton)
+    @chat = chats(:one)
+  end
+
+  test "should not save message without content" do
+    message = Message.new(user: @user, chat: @chat)
+    assert_not message.save, "Saved the message without content"
+  end
+
+  test "should save message with content" do
+    message = Message.new(content: "Test Content", user: @user, chat: @chat)
+    assert message.save, "Failed to save the message with content"
+  end
+
+  test "should belong to user" do
+    message = Message.new(content: "Test Content", user: @user, chat: @chat)
+    assert_respond_to message, :user
+  end
+
+  test "should belong to chat" do
+    message = Message.new(content: "Test Content", user: @user, chat: @chat)
+    assert_respond_to message, :chat
+  end
 end
