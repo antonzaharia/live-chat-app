8243ce0b4ae53442dec048619e9b73a8cace90e6
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 13:08:01 +0000

    feat: generated Message resource

---
+++ app/models/message.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  user_id    :bigint           not null
+#  chat_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Model representing a message sent within a chat room.
+class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :chat
+end
