231e92f04ee525e1c1068c1cbe2f485592a186b5
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 15:13:52 +0000

    feat: Leave conversation
    Changes:
    - added destroy action for user chats

--- app/controllers/user_chats_controller.rb
+++ app/controllers/user_chats_controller.rb
@@ -1,2 +1,17 @@
 class UserChatsController < ApplicationController
+  before_action :set_user_chat
+
+  def destroy
+    @user_chat.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: "You've left the conversation." }
+    end
+  end
+
+  private
+
+  def set_user_chat
+    @user_chat = Current.user.user_chats.find(params[:id])
+  end
 end
