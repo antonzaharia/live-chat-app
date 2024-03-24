ca712a0f8bc793d21ae4870250d9c75a2c7bb301
antonzaharia <zaharia_anton@yahoo.com>
2024-03-24 13:07:44 +0000

    feat: generated Chats resource

---
+++ app/models/chat.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: chats
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Model representing a chat room where users can exchange messages.
+class Chat < ApplicationRecord
+  has_many :messages, dependent: :destroy
+  has_many :user_chats, dependent: :destroy
+  has_many :users, through: :user_chats
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :chats
   get  "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
   get  "sign_up", to: "registrations#new"
